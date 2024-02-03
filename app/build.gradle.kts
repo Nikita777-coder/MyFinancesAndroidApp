32dc44c3307b293a6f07ccbeb99e2d1d433a98bf
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-02-03 22:21:11 +0300

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,17 @@ android {
 }
 
 dependencies {
+//    implementation("org.slf4j:slf4j-api:2.0.11")
+//    implementation("org.slf4j:slf4j-simple:1.7.30")
+//    implementation("org.apache.logging.log4j:log4j-api:2.22.1")
+//    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
+//    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.22.1")
+//    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    implementation("org.projectlombok:lombok:1.18.30")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
