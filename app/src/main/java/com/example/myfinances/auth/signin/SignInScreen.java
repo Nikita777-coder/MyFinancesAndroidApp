be3447948586cada324fbfc6519fa761b59fe04f
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-02-03 22:22:16 +0300

    add not tested version which maybe work

--- app/src/main/java/com/example/myfinances/auth/signin/SignInScreen.java
+++ app/src/main/java/com/example/myfinances/auth/signin/SignInScreen.java
@@ -8,19 +8,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.myfinances.R;
-import com.example.myfinances.auth.SignUpScreen;
 import com.example.myfinances.auth.signin.reactions.HttpStatusBadRequestReaction;
 import com.example.myfinances.auth.signin.reactions.HttpStatusOkReaction;
 import com.example.myfinances.auth.signin.reactions.Reaction;
+import com.example.myfinances.auth.signup.SignUpScreen;
+import com.example.myfinances.services.AuthServiceService;
+import com.example.myfinances.services.auth.mappers.SignInMapper;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import retrofit2.Response;
 
 public class SignInScreen extends AppCompatActivity {
-    private final AuthServiceSender authServiceSender;
-    private final Map<HttpStatusCode, Reaction> httpStatusesReactions = new HashMap<Integer, Reaction>() {{
-        put(HttpStatus.OK, new HttpStatusOkReaction());
-        put(HttpStatus.BadReques, new tHttpStatusBadRequestReaction());
+    private final Map<Integer, Reaction> httpStatusesReactions = new HashMap<Integer, Reaction>() {{
+        put(200, new HttpStatusOkReaction());
+        put(400, new HttpStatusBadRequestReaction());
     }};
 
     @Override
@@ -36,7 +40,13 @@ public class SignInScreen extends AppCompatActivity {
                 startActivity(new Intent(SignInScreen.this, SignUpScreen.class))
         );
 
-        AuthResponse authResponse = authServiceSender.signIn(AuthRequest.Builder.publicData(publicData).password(password).build());
-        httpStatusesReactions.get(authResponse.getHttpStatusCode()).handle(authResponse.getMessage());
+        Response<String> response = AuthServiceService.signIn(
+                SignInMapper.SIGN_IN_MAPPER.userDataToSignInRequest(
+                        publicData.getText().toString(),
+                        password.getText().toString()
+                )
+        );
+
+        Objects.requireNonNull(httpStatusesReactions.get(response.code())).handle(response.body());
     }
 }
