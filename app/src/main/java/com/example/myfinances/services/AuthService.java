88f3e38abe78ff0602a90354427ad7b5af544568
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-02-23 00:35:01 +0300

    enqueue call changes to execute call in signIn, but it doesn't bring any affect with server connection

--- app/src/main/java/com/example/myfinances/services/AuthService.java
+++ app/src/main/java/com/example/myfinances/services/AuthService.java
@@ -5,6 +5,8 @@ import com.example.myfinances.services.auth.dto.SignInRequest;
 import com.example.myfinances.services.auth.dto.SignUpRequest;
 import com.example.myfinances.services.auth.dto.UserOutData;
 
+import java.io.IOException;
+
 import lombok.experimental.UtilityClass;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,23 +17,45 @@ import timber.log.Timber;
 public class AuthService {
     private final static AuthApi AUTH_SERVICE_SERVICE = (new Service<>(AuthApi.class, "http://10.0.2.2:8080/")).getController();
     public static Response<String> signIn(SignInRequest signInRequest) {
-        final Response<String>[] responseOut = new Response[]{null};
+//        final Response<String>[] responseOut = new Response[]{null};
+//
+//        Callback<String> callback = new Callback<>() {
+//            @Override
+//            public void onResponse(Call<String> call, Response<String> response) {
+//                responseOut[0] = response;
+//            }
+//
+//            @Override
+//            public void onFailure(Call<String> call, Throwable t) {
+//
+//            }
+//        };
+//
+//        signIn(signInRequest, callback);
+//
+//        return responseOut[0];
 
+        try {
+            return AUTH_SERVICE_SERVICE.signIn(signInRequest).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void signIn(SignInRequest signInRequest, Callback<String> callback) {
         AUTH_SERVICE_SERVICE.signIn(signInRequest)
                 .enqueue(new Callback<>() {
                     @Override
                     public void onResponse(Call<String> call, Response<String> response) {
                         Timber.i("signIn response got");
-                        responseOut[0] = response;
+                        callback.onResponse(call, response);
                     }
 
                     @Override
                     public void onFailure(Call<String> call, Throwable t) {
                         Timber.e(t);
+                        callback.onFailure(call, t);
                     }
                 });
-
-        return responseOut[0];
     }
     public static Response<UserOutData> signUp(SignUpRequest signUpRequest) {
         final Response<UserOutData>[] responseOut = new Response[]{null};
