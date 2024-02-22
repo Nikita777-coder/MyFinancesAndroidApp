dc3dbaef3defdebff75de04c745ce769b2823473
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-02-23 00:33:29 +0300

    manual setters changed to lombok setters

--- app/src/main/java/com/example/myfinances/services/auth/dto/SignInRequest.java
+++ app/src/main/java/com/example/myfinances/services/auth/dto/SignInRequest.java
@@ -8,20 +8,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class SignInRequest {
     private String login;
     private String email;
     private String password;
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
