8044822d997441a0f355608805963cc8db31e222
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-04-16 00:29:54 +0300

    create start screen

--- app/src/main/java/com/example/myfinances/activities/MainActivity.java
+++ app/src/main/java/com/example/myfinances/activities/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.myfinances.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.myfinances.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int TIME_OUT = 2000; //Time to launch the another activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, SignInScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
